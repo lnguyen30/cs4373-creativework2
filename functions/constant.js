bceb8de15edbc643e4bfacdf17e7b10bcffcd2b2
Long Nguyen <lnguyen30@uco.edu>
2021-06-13 23:34:13 -0500

    checks isAdmin, added collections in indexjs,

--- functions/constant.js
+++ functions/constant.js
@@ -1,5 +1,12 @@
 exports.DEV = true;
 
+// export admin email outside for functions
 exports.adminEmails = [
     'admin@test.com'
 ]
+
+//collection name for replies in functions
+exports.collectionNames= {
+    THREADS: 'threads',
+    REPLIES: 'replies',
+}
--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,6 @@
 const functions = require("firebase-functions");
 
-
+//imports admin key
 const admin = require("firebase-admin");
 
 const serviceAccount = require("./account_key.json");
@@ -9,9 +9,28 @@ admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
 
+//import Constant js file
+const Constant = require('./constant.js')
+
+//functions to export to client side
+
+//checks if user is admin
+function isAdmin(email){
+    return Constant.adminEmails.includes(email);
+}
 
 // cloud function to retrieve replies by docid
 async function getReplyById(data, context){
-    // implement user check?
-    
+    //checks if admin is signed in
+   if (!isAdmin(context.auth.token.email)){
+       if(Constant.DEV) console.log('not admin', context.auth.token.email)
+       throw new functions.https.HttpsError('unauthenticated', 'Only admins may update this')
+   }
+
+    try{
+
+    }catch(e){
+        if(Constant.DEV) console.log(e)
+        throw new functions.https.HttpsError('internal', 'getReplyById Failed')
+    }
 }
